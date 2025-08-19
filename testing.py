383ded6841c2f9e3962ecbd2a6db084e46020968
Jibran Sajid <jibransajid24@gmail.com>
2025-08-19 11:52:18 +0100

    Moved everything in main function

--- testing.py
+++ testing.py
@@ -1,4 +1,9 @@
-name = input("Your name? ").strip()
-house = input("Your house? ").strip()
+def main():
+    name = input("Your name? ").strip()
+    house = input("Your house? ").strip()
 
-print(f"Hello, {name} from {house}")
+    print(f"Hello, {name} from {house}")
+
+
+if __name__ == "__main__":
+    main()
