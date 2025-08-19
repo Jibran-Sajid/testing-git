5ee5b3f9c3fb47a132cfbd46466e3a1309d3d335
Jibran Sajid <jibransajid24@gmail.com>
2025-08-19 11:46:58 +0100

    Added new feature to the Patronus

--- testing.py
+++ testing.py
@@ -1,4 +1,22 @@
-name = input("Your name? ").strip()
-house = input("Your house? ").strip()
+def main():
+    name = input("Your name? ").strip()
+    house = input("Your house? ").strip()
+    patronus = get_patronus(name)
 
-print(f"Hello, {name} from {house}")
+    print(f"Hello, {name} from {house}")
+    print("Your patronus is", patronus)
+
+
+def get_patronus(name):
+    if name == "Harry":
+        return "🐎"
+    elif name == "Ron":
+        return "🦁"
+    elif name == "Hermione":
+        return "🦈"
+    else:
+        return "🪄"
+
+        
+if __name__ == "__main__":
+    main()
